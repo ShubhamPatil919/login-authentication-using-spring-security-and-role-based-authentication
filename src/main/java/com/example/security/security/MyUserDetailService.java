f47fa3414d5637caa7342a4f9e30eae305a7b4ec
ShubhamPatil919 <patilshubham919@gmail.com>
2020-05-21 11:49:00 +0530

    commited on 21.05.2020

---
+++ src/main/java/com/example/security/security/MyUserDetailService.java
@@ -0,0 +1,28 @@
+package com.example.security.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.security.entity.MyUser;
+import com.example.security.entity.User;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+	@Autowired
+	MyUser myuser;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+		Optional<User> user = myuser.findByUserName(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException("Not Found" + userName));
+		return user.map(MyUserDetails::new).get();
+	}
+
+}
